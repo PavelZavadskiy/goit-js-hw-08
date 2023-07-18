d14f73de6322b5dad664a32471b101a74c2c882a
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-07-18 18:59:41 +0300

    Added Video playback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? '0' : JSON.parse(serializedState);
+  } catch (error) {
+    return '0';
+  }
+};
+
+player
+  .setCurrentTime(parseFloat(load(VIDEOPLAYER_CURRENT_TIME)))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('timeupdate', function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, data.seconds.toString());
+});
